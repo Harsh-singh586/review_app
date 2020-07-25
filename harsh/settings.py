94f46f7c141dfb1fafdf8eb58645ebd1f193a345
Harsh kumar Singh <tacc91003@gmail.com>
2020-07-25 20:03:56 +0530

    Update settings.py

--- harsh/settings.py
+++ harsh/settings.py
@@ -20,12 +20,12 @@ TEMP_DIR = os.path.join(BASE_DIR,"template")
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6*us_tz20!%#=ik=3+m$cn(p*zj@qcqiop3l7rg^q-qy+o!*4f'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['HarshSingh.pythonanywhere.com']
+
 
 
 # Application definition
